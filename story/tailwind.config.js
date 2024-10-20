54b8d872d7b975dddb78333f81bf3270ce2c9cb2
ishan9669-create <davv9669@gmail.com>
2024-10-20 12:20:06 +0530

    Update tailwind.config.js

--- story/tailwind.config.js
+++ story/tailwind.config.js
@@ -3,7 +3,35 @@ export default {
   content: [ "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      keyframes:{
+        rightlao:{
+          '0%':{
+            transform:'translateX(100%)',
+            opacity:'0'
+          },
+          '100%':{
+            transform:'translateX(0px)',
+            opacity:'1'
+          }
+        },
+        leftlao:{
+          '0%':{
+            transform:'translateX(0px)',
+            opacity:'1'
+          },
+          '100%':{
+            transform:'translateX(100%)',
+            opacity:'0'
+          }
+        }
+
+      },
+      animation:{
+        rightlao:'rightlao 0.3s ease-in forwards',
+        leftlao:'leftlao 0.3s ease-in forwards'
+      }
+    },
   },
   plugins: [],
 }
