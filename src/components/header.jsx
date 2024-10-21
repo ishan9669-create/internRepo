f6ff04fd8a5a8fb527b3515038953c29f886e40f
ishan9669-create <davv9669@gmail.com>
2024-10-21 19:33:15 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -19,20 +19,31 @@ const Navbar = () => {
         <img
           src="./Horizontal _Neo CFO_ Logo _ coloured@2x 1.png"
           alt="Logo"
-          className="w-32"
-        />
+          className="w-32 cursor-pointer"
+        onClick={()=>{
+          window.location.href = '/'
+        }}/>
       </div>
 
       {/* Menu Section for large screens */}
       <div className="hidden md:flex justify-center gap-16">
-        <span>Our Story</span>
-        <span>Features</span>
-        <span>Blogs</span>
+        <span className='cursor-pointer' onClick={()=>{
+          window.location.href = '/story'
+        }}>Our Story</span>
+        <span className='cursor-pointer' onClick={()=>{
+          window.location.href = '/feature'
+        }}>Features</span>
+        <span className='cursor-pointer' onClick={()=>{
+          window.location.href = '/blogpage'
+        }}>Blogs</span>
       </div>
 
       {/* Schedule a Demo for large screens */}
       <div className="hidden md:flex justify-center items-center underline">
-        <a href="#">Schedule a Demo </a>
+        <a className='cursor-pointer' onClick={(e)=>{
+          e.preventDefault();
+          window.location.href = '/schedule'
+        }}>Schedule a Demo </a>
         <GoArrowUpRight />
       </div>
 
@@ -53,13 +64,23 @@ const Navbar = () => {
       {(
         <div className={`absolute top-16 h-[550px] left-0 w-full text-white bg-black bg-opacity-90 flex flex-col justify-between shadow-xl md:hidden ${isMenuOpen ? "animate-rightlao":'animate-leftlao'}`}>
         <div className='ml-7'>
-          <a href="#" className="p-4 items-center flex w-full text-center">
+          <a href="#"
+          onClick={(e)=>{
+            e.preventDefault()
+            window.location.href = '/story'
+          }} className="p-4 items-center flex w-full text-center">
             Our Story
           </a>
-          <a href="#" className="p-4 flex w-full text-center">
+          <a href="#" onClick={(e)=>{
+            e.preventDefault()
+            window.location.href = '/feature'
+          }} className="p-4 flex w-full text-center">
             Features
           </a>
-          <a href="#" className="p-4 flex w-full text-center">
+          <a href="#" onClick={(e)=>{
+            e.preventDefault()
+            window.location.href = '/blogpage'
+          }} className="p-4 flex w-full text-center">
             Blogs
           </a>
         </div>
@@ -68,7 +89,9 @@ const Navbar = () => {
           <p className='w-[200px] rounded-lg font-semibold'>
             Transform Your Financial Processes with Neo CFO
           </p>
-          <button className="py-3 rounded-lg text-white bg-black px-2 w-[180px] text-center underline flex justify-center items-center">
+          <button onClick={()=>{
+            window.location.href = '/schedule'
+          }} className="py-3 rounded-lg text-white bg-black px-2 w-[180px] text-center underline flex justify-center items-center">
             Schedule a Demo
             <GoArrowUpRight />
           </button>
