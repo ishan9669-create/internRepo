aeac1c8bce5bf28a3d0829233c218d32cd8e4bbb
ishan9669-create <davv9669@gmail.com>
2024-10-22 10:24:30 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import Header from './components/header.jsx'
 import Footer from './components/footer.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  
-    <App />
+  <BrowserRouter> <App /></BrowserRouter>
+   
   
 )
