2cebf6c315aac2a565b27515a94ee279d4928961
ishan9669-create <davv9669@gmail.com>
2024-10-25 20:51:57 +0530

    Update blogpage.jsx

--- src/pages/blogpage.jsx
+++ src/pages/blogpage.jsx
@@ -13,8 +13,8 @@
       <h2 class="text-sm sm:text-md md:text-md leading-tight">Blogs</h2>
   </div>
 
-        <h1 class="text-3xl sm:text-4xl md:text-5xl lg:text-3xl font-bold mb-6 md:mb-10 w-full lg:w-[939px] lg:h-auto "
-      style={{fontFamily: 'Geist', fontWeight: '400', letterSpacing: '-0.04em', color: '#000000', transform: 'rotate(-0.08deg)', lineHeight: '1.2'}}>
+        <h1 class="text-3xl font-geist sm:text-4xl md:text-5xl lg:text-3xl font-bold mb-6 md:mb-10 w-full lg:w-[939px] lg:h-auto "
+      style={{ fontWeight: '400', letterSpacing: '-0.04em', color: '#000000', transform: 'rotate(-0.08deg)', lineHeight: '1.2'}}>
       Insights & Innovations:
       <br/>Let's Explore the <i className='text-3xl sm:text-4xl md:text-5xl lg:text-3xl '>Future of Finance</i>
   </h1>
@@ -28,108 +28,7 @@
   </div>
 
 
-        {/* <div className="grid grid-cols-2 lg:grid-cols-3 gap-3 mt-32">
-
-          <div className="relative bg-white shadow-lg rounded-lg overflow-hidden"
-            style={{backgroundImage: "url('./dylan-calluy-JpflvzEl5cg-unsplash.jpg')", backgroundSize: 'cover', backgroundPosition: "center", height: "300px"}}>
-            <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-            <div className="absolute top-2 left-2 text-white text-md z-10">Finance</div>
-            <div className="absolute bottom-2 left-6 right-6 z-10">
-              <h2 className="text-xl font-semibold mb-4 text-white">Top 5 Ways to Revolutionize Your Accounting Processes
-              </h2>
-              <p className="text-gray-200 mb-2">5 min read</p>
-            </div>
-          </div>
-          <div className="relative bg-white shadow-lg rounded-lg overflow-hidden"
-          style={{backgroundImage: "url('./austin-distel-744oGeqpxPQ-unsplash.jpg')", backgroundSize: 'cover', backgroundPosition: "center", height: "300px"}}
-          >
-            <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-            <div className="absolute top-2 left-2 text-white text-lg z-10">Banking</div>
-            <div className="absolute bottom-2 left-6 right-6 z-10">
-              <h2 className="text-2xl font-semibold mb-4 text-white">The Ultimate Guide to
-                Maximizing Efficiency</h2>
-              <p className="text-gray-200 mb-2">10 min read</p>
-            </div>
-          </div>
-
-          <div className="relative bg-white shadow-lg rounded-lg overflow-hidden"
-            style={{backgroundImage: "url('./photo1.png')", backgroundSize: 'cover',backgroundPosition: 'center', height: '300px'}}>
-            <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-            <div className="absolute top-2 left-2 text-white text-lg z-10">Lorem</div>
-            <div className="absolute bottom-2 left-6 right-6 z-10">
-              <h2 className="text-2xl font-semibold mb-4 text-white">The Future of Financial Automation</h2>
-              <p className="text-gray-200 mb-2">8 min read</p>
-            </div>
-          </div>
-
-          <div className="relative bg-white shadow-lg rounded-lg overflow-hidden"
-            style={{backgroundImage: "url('./photo1.png')", backgroundSize: 'cover',backgroundPosition: 'center', height: '300px'}}>
-            <div className="absolute inset-0 bg-black bg-opacity-30"></div>
-            <div className="absolute top-2 left-2 text-white text-lg font-bold z-10">Lorem</div>
-            <div className="absolute bottom-2 left-6 right-6 z-10">
-              <h2 className="text-2xl font-semibold mb-4 text-white">The Future of Financial Automation</h2>
-              <p className="text-gray-200 mb-2">8 min read</p>
-            </div>
-          </div>
-
-          <div className="relative bg-white shadow-lg rounded-lg overflow-hidden"
-            style={{backgroundImage: "url('./dylan-calluy-JpflvzEl5cg-unsplash.jpg')", backgroundSize: 'cover', backgroundPosition: 'center', height: '300px'}}>
-            <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-            <div className="absolute top-2 left-2 text-white text-lg z-10">Finance</div>
-            <div className="absolute bottom-2 left-6 right-6 z-10">
-              <h2 className="text-2xl font-semibold mb-4 text-white">Top 5 Ways to Revolutionize Your Accounting Processes
-              </h2>
-              <p className="text-gray-200 mb-2">5 min read</p>
-            </div>
-          </div>
-
-          <div className="relative bg-white shadow-lg rounded-lg overflow-hidden"
-            style={{backgroundImage: "url('./austin-distel-744oGeqpxPQ-unsplash.jpg')",backgroundSize: 'cover', backgroundPosition: 'center', height: '300px'}}>
-            <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-            <div className="absolute top-2 left-2 text-white text-lg font-bold z-10">Finance</div>
-            <div className="absolute bottom-2 left-6 right-6 z-10">
-              <h2 className="text-2xl font-semibold mb-4 text-white">Top 5 Ways to Revolutionize Your Accounting Processes
-              </h2>
-              <p className="text-gray-200 mb-2">5 min read</p>
-            </div>
-          </div>
-
-          <div className="relative bg-white shadow-lg rounded-lg overflow-hidden"
-          style={{backgroundImage: "url('./dylan-calluy-JpflvzEl5cg-unsplash.jpg')",backgroundSize: 'cover', backgroundPosition: 'center', height: '300px'}}
-            >
-            <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-            <div className="absolute top-2 left-2 text-white text-lg z-10">Finance</div>
-            <div className="absolute bottom-2 left-6 right-6 z-10">
-              <h2 className="text-2xl font-semibold mb-4 text-white">Top 5 Ways to Revolutionize Your Accounting Processes
-              </h2>
-              <p className="text-gray-200 mb-2">5 min read</p>
-            </div>
-          </div>
-
-          <div className="relative bg-white shadow-lg rounded-lg overflow-hidden"
-          style={{backgroundImage: "url('./austin-distel-744oGeqpxPQ-unsplash.jpg')",backgroundSize: 'cover', backgroundPosition: 'center', height: '300px'}}
-            >
-            <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-            <div className="absolute top-2 left-2 text-white text-lg font-bold z-10">Finance</div>
-            <div className="absolute bottom-2 left-6 right-6 z-10">
-              <h2 className="text-2xl font-semibold mb-4 text-white">Top 5 Ways to Revolutionize Your Accounting Processes
-              </h2>
-              <p className="text-gray-200 mb-2">5 min read</p>
-            </div>
-          </div>
-
-          <div className="relative bg-white shadow-lg rounded-lg overflow-hidden"
-          style={{backgroundImage: "url('./photo1.png')",backgroundSize: 'cover', backgroundPosition: 'center', height: '300px'}}
-        >
-            <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-            <div className="absolute top-2 left-2 text-white text-lg font-bold z-10">Finance</div>
-            <div className="absolute bottom-2 left-6 right-6 z-10">
-              <h2 className="text-2xl font-semibold mb-4 text-white">Top 5 Ways to Revolutionize Your Accounting Processes
-              </h2>
-              <p className="text-gray-200 mb-2">5 min read</p>
-            </div>
-          </div>
-        </div> */}
+       
     <div className="flex flex-col items-center  w-full sm:grid   sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-32">
     {/* Card 1 */}
     <div
@@ -345,14 +244,14 @@
     </main> 
 
 
-   <div className='w-full  flex justify-center mt-20'>
-       <div id="demo"  >
-        <h1 className='text-xs md:text-2xl'>Transform Your Financial <br/>
-            Processes with Neo CFO</h1>
-        <button  className="flex items-center bg-white px-2 py-1 rounded-md">Schedule a Demo <GoArrowUpRight/></button>
+    <div className='w-full  flex justify-center'>
+        <div id="demo" style={{marginLeft:'60px',marginRight:'40px'}} >
+          <h1 className='text-xs md:text-2xl'>Transform Your Financial <br/>
+              Processes with Neo CFO</h1>
+          <button  className="flex items-center bg-white px-2 py-1 rounded-md" >Schedule a Demo <GoArrowUpRight/></button>
 
-    </div>
-       </div>
+      </div>
+        </div>
   
 
     <Footer/>
